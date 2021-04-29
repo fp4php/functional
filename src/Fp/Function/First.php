1f646f5d57b31c469d2f8cbea871dbe553c2224f
whsv26 <whsv26@gmail.com>
2021-04-29 22:48:23 +0300

    First and head functions.

---
+++ src/Fp/Function/First.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp\Function;
+
+use Fp\Functional\Option\Option;
+
+/**
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param iterable<TK, TV> $collection
+ * @psalm-param null|\Closure(TV, TK): bool $callback
+ *
+ * @psalm-return Option<TV>
+ */
+function first(iterable $collection, ?callable $callback = null): Option
+{
+    if (is_null($callback)) {
+        return head($collection);
+    }
+
+    $first = null;
+
+    foreach ($collection as $index => $element) {
+        if ($callback($element, $index)) {
+            $first = $element;
+            break;
+        }
+    }
+
+    return Option::of($first);
+}
---
+++ src/Fp/Function/Head.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp\Function;
+
+use Fp\Functional\Option\Option;
+
+/**
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param iterable<TK, TV> $collection
+ *
+ * @psalm-return Option<TV>
+ */
+function head(iterable $collection): Option
+{
+    $head = null;
+
+    foreach ($collection as $element) {
+        $head = $element;
+        break;
+    }
+
+    return Option::of($head);
+}
