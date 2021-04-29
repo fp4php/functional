c7f2c7df567949e9028612fc56a74d93cb552957
whsv26 <whsv26@gmail.com>
2021-04-29 22:54:03 +0300

    Curry 4 and 5.

--- src/Fp/Function/Curry.php
+++ src/Fp/Function/Curry.php
@@ -49,3 +49,38 @@ function curry3(callable $callback, mixed $in1): callable
     return fn(mixed $in2, mixed $in3) => $callback($in1, $in2, $in3);
 }
 
+/**
+ * @psalm-template TI1
+ * @psalm-template TI2
+ * @psalm-template TI3
+ * @psalm-template TI4
+ * @psalm-template TO
+ *
+ * @psalm-param callable(TI1, TI2, TI3, TI4): TO $callback
+ * @psalm-param TI1 $in1
+ *
+ * @psalm-return callable(TI2, TI3, TI4): TO
+ */
+function curry4(callable $callback, mixed $in1): callable
+{
+    return fn(mixed $in2, mixed $in3, mixed $in4) => $callback($in1, $in2, $in3, $in4);
+}
+
+/**
+ * @psalm-template TI1
+ * @psalm-template TI2
+ * @psalm-template TI3
+ * @psalm-template TI4
+ * @psalm-template TI5
+ * @psalm-template TO
+ *
+ * @psalm-param callable(TI1, TI2, TI3, TI4, TI5): TO $callback
+ * @psalm-param TI1 $in1
+ *
+ * @psalm-return callable(TI2, TI3, TI4, TI5): TO
+ */
+function curry5(callable $callback, mixed $in1): callable
+{
+    return fn(mixed $in2, mixed $in3, mixed $in4, mixed $in5) => $callback($in1, $in2, $in3, $in4, $in5);
+}
+
