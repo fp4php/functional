82e0ae2f97c53a277e767d1300026681a9afdf95
whsv26 <whsv26@gmail.com>
2021-05-03 23:57:39 +0300

    Every function support.

---
+++ src/Fp/Function/Every.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp\Function;
+
+/**
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param iterable<TK, TV> $collection
+ * @psalm-param \Closure(TV, TK, iterable<TK, TV>): bool $callback
+ *
+ * @psalm-return bool
+ */
+function every($collection, callable $callback, bool $strict = true): bool
+{
+    $result = !$strict;
+
+    foreach ($collection as $index => $element) {
+        $result = true;
+
+        if (!$callback($element, $index, $collection)) {
+            $result = false;
+            break;
+        }
+    }
+
+    return $result;
+}
