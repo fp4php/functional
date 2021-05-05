4d7b9ea3245716f3cd18420b3a18aa09b94af302
whsv26 <whsv26@gmail.com>
2021-05-05 21:18:42 +0300

    Second element function.

---
+++ src/Fp/Function/Second.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp\Function;
+
+use Fp\Functional\Option\Option;
+
+/**
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param iterable<TK, TV> $collection
+ * @psalm-return Option<TV>
+ */
+function second(iterable $collection): Option
+{
+    return head(tail($collection));
+}
