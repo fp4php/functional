84fc8d0a4001c01f5aabd4405f902a79eae569f7
whsv26 <whsv26@gmail.com>
2021-05-10 18:06:59 +0300

    Json search bug fix.

--- src/Fp/Function/Json/JsonSearch.php
+++ src/Fp/Function/Json/JsonSearch.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Fp\Function\Json;
 
+use Fp\Functional\Either\Right;
 use Fp\Functional\Option\Option;
 
 use function JmesPath\search;
@@ -13,10 +14,16 @@ use function JmesPath\search;
  */
 function jsonSearch(string $expr, array|string $data): Option
 {
-    $decoded = is_string($data) ? jsonDecode($data) : $data;
+    $decoded = is_string($data)
+        ? jsonDecode($data)
+        : Right::of($data);
 
-    /** @var array|scalar|null $result */
-    $result = search($expr, $decoded);
+    return $decoded
+        ->toOption()
+        ->map(function (array $decoded) use ($expr) {
+            /** @var array|scalar|null $result */
+            $result = search($expr, $decoded);
 
-    return Option::of($result);
+            return $result;
+        });
 }
