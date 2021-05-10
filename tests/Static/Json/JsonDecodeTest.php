dde853283a705fa20c6b812300e1034f8277384b
whsv26 <whsv26@gmail.com>
2021-05-11 00:35:34 +0300

    Json functions tests.

---
+++ tests/Static/Json/JsonDecodeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Static\Json;
+
+use Fp\Functional\Either\Either;
+use Tests\PhpBlockTestCase;
+
+final class JsonDecodeTest extends PhpBlockTestCase
+{
+    public function testDecode(): void
+    {
+        $phpBlock = /** @lang InjectablePHP */ '
+            $json = "{}";
+            $result = \Fp\Json\jsonDecode($json);
+        ';
+
+        $this->assertBlockType($phpBlock, Either::class . '<string, array<array-key, mixed>>');
+    }
+}
