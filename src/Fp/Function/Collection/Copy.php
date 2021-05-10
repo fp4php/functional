7e649a9fbb48ed883137b985dcd7f039f9ee5d92
whsv26 <whsv26@gmail.com>
2021-05-10 19:17:49 +0300

    Copy iterable function.

---
+++ src/Fp/Function/Collection/Copy.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp\Function\Collection;
+
+/**
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param iterable<TK, TV> $collection
+ *
+ * @psalm-return array<TK, TV>
+ */
+function copy(iterable $collection): array
+{
+    $aggregation = [];
+
+    foreach ($collection as $index => $element) {
+        $aggregation[$index] = $element;
+    }
+
+    return $aggregation;
+}
