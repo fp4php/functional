50dcf43c3b52be6ed74a6d39fa712aff2674123e
whsv26 <whsv26@gmail.com>
2021-05-10 23:37:29 +0300

    First function test.

--- tests/Static/Collection/FirstTest.php
+++ tests/Static/Collection/FirstTest.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\Static\Collection;
 
+use Fp\Functional\Option\Option;
 use Tests\PhpBlockTestCase;
 
-final class FilterTest extends PhpBlockTestCase
+final class FirstTest extends PhpBlockTestCase
 {
     public function testWithArray(): void
     {
@@ -16,12 +17,12 @@ final class FilterTest extends PhpBlockTestCase
              */
             function getCollection(): array { return []; }
             
-            $result = \Fp\Collection\filter(
+            $result = \Fp\Collection\first(
                 getCollection(),
                 fn(int $v, string $k) => true
             );
         ';
 
-        $this->assertBlockType($phpBlock, 'array<string, int>');
+        $this->assertBlockType($phpBlock, Option::class . '<int>');
     }
 }
