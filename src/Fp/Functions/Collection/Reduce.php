b7357da56364be53bfb7e5e24e8682a82be10828
whsv26 <whsv26@gmail.com>
2021-05-21 00:22:55 +0300

    Reduce nel and ner.

--- src/Fp/Functions/Collection/Reduce.php
+++ src/Fp/Functions/Collection/Reduce.php
@@ -6,6 +6,8 @@ namespace Fp\Collection;
 
 use Fp\Functional\Option\Option;
 
+use function Fp\Cast\asNonEmptyList;
+
 /**
  * Reduce multiple elements into one
  * Returns None for empty collection
@@ -20,17 +22,45 @@ use Fp\Functional\Option\Option;
  */
 function reduce(iterable $collection, callable $callback): Option
 {
-    $tail = tail($collection);
+    return Option::do(function () use ($collection, $callback) {
+        $nel = yield asNonEmptyList($collection);
+        return reduceNel($nel, $callback);
+    });
+}
 
-    return head($collection)
-        ->map(function (mixed $head) use ($tail, $callback): mixed {
-            /** @var TV $acc */
-            $acc = $head;
+/**
+ * Reduce non-empty-list into one value
+ *
+ * @psalm-template TV
+ *
+ * @psalm-param non-empty-list<TV> $collection
+ * @psalm-param callable(TV, TV): TV $callback (accumulator, current value): new accumulator
+ *
+ * @psalm-return TV
+ */
+function reduceNel(array $collection, callable $callback): mixed
+{
+    return reduceNer($collection, $callback);
+}
 
-            foreach ($tail as $element) {
-                $acc = call_user_func($callback, $acc, $element);
-            }
+/**
+ * Reduce non-empty-array into one value
+ *
+ * @psalm-template TK of array-key
+ * @psalm-template TV
+ *
+ * @psalm-param non-empty-array<TK, TV> $collection
+ * @psalm-param callable(TV, TV): TV $callback (accumulator, current value): new accumulator
+ *
+ * @psalm-return TV
+ */
+function reduceNer(array $collection, callable $callback): mixed
+{
+    $acc = array_shift($collection);
 
-            return $acc;
-        });
+    foreach ($collection as $element) {
+        $acc = call_user_func($callback, $acc, $element);
+    }
+
+    return $acc;
 }
