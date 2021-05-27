9b6bb6a2a01e51bcdf313c815656bcbdd7df994e
zaur <zaur.nadiraliev.228@gmail.com>
2021-05-27 22:35:19 +0300

    group function - better type inference group key

--- tests/Static/Functions/Collection/GroupTest.php
+++ tests/Static/Functions/Collection/GroupTest.php
@@ -15,13 +15,65 @@ final class GroupTest extends PhpBlockTestCase
              * @psalm-return array<string, int> 
              */
             function getCollection(): array { return []; }
-            
+
             $result = \Fp\Collection\group(
                 getCollection(),
-                fn(int $v, string $k) => $k . "10"
+                fn(int $v, string $k) => /** @var array-key */ $k . "10"
             );
         ';
 
-        $this->assertBlockType($phpBlock, 'array<array-key, array<string, int>>');
+        $this->assertBlockType($phpBlock, 'array<non-empty-string, array<string, int>>');
+    }
+
+    public function testWithListInferGroupKey(): void
+    {
+        $phpBlock = /** @lang InjectablePHP */ '
+            /** 
+             * @psalm-return list<string> 
+             */
+            function getCollection(): array { return []; }
+            
+            $result = \Fp\Collection\group(
+                getCollection(),
+                fn(string $value) => $value . "10"
+            );
+        ';
+
+        $this->assertBlockType($phpBlock, 'array<non-empty-string, array<int, string>>');
+    }
+
+    public function testWithArrayInferGroupKey(): void
+    {
+        $phpBlock = /** @lang InjectablePHP */ '
+            /** 
+             * @psalm-return array<non-empty-string, string>
+             */
+            function getCollection(): array { return []; }
+            
+            $result = \Fp\Collection\group(
+                getCollection(),
+                fn(string $value) => $value . "10"
+            );
+        ';
+
+        $this->assertBlockType($phpBlock, 'array<non-empty-string, array<non-empty-string, string>>');
+    }
+
+    public function testWithArrayAndGroupKeyAsTypeAlias()
+    {
+        $phpBlock = /** @lang InjectablePHP */ '
+            /** 
+             * @psalm-type Alias = string
+             * @psalm-return array<Alias, int>
+             */
+            function getCollection(): array { return []; }
+            
+            $result = \Fp\Collection\group(
+                getCollection(),
+                fn(string $value) => $value
+            );
+        ';
+
+        $this->assertBlockType($phpBlock, 'array<string, array<string, int>>');
     }
 }
