8feaa19c201ab669b76cf7a10edae3bd74f4ea72
whsv26 <whsv26@gmail.com>
2021-08-12 00:26:27 +0300

    Sequence operation signatures.

--- src/Fp/Collections/NonEmptySeqOps.php
+++ src/Fp/Collections/NonEmptySeqOps.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Fp\Collections;
 
+use Fp\Functional\Option\Option;
+
 /**
  * @psalm-immutable
  * @template-covariant TV
@@ -11,9 +13,117 @@ namespace Fp\Collections;
 interface NonEmptySeqOps
 {
     /**
+     * Returns true if there is collection element of given class
+     * False otherwise
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    function anyOf(string $fqcn, bool $invariant = false): bool;
+
+    /**
+     * Find element by its index
+     * Returns None if there is no such collection element
+     *
+     * @psalm-return Option<TV>
+     */
+    function at(int $index): Option;
+
+    /**
+     * Returns true if every collection element satisfy the condition
+     * false otherwise
+     *
+     * @psalm-param callable(TV): bool $predicate
+     */
+    function every(callable $predicate): bool;
+
+    /**
+     * Returns true if every collection element is of given class
+     * false otherwise
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    function everyOf(string $fqcn, bool $invariant = false): bool;
+
+    /**
+     * Find if there is element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     */
+    function exists(callable $predicate): bool;
+
+    /**
+     * Filter collection by condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Seq<TV>
+     */
+    function filter(callable $predicate): Seq;
+
+    /**
+     * Filter not null elements
+     *
+     * @psalm-return Seq<TV>
+     */
+    function filterNotNull(): Seq;
+
+    /**
+     * Filter elements of given class
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     * @psalm-return Seq<TVO>
+     */
+    function filterOf(string $fqcn, bool $invariant = false): Seq;
+
+    /**
+     * Find first element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    function first(callable $predicate): Option;
+
+    /**
+     * Find first element of given class
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     * @psalm-return Option<TVO>
+     */
+    function firstOf(string $fqcn, bool $invariant = false): Option;
+
+    /**
+     * @psalm-template TVO
+     * @psalm-param callable(TV): NonEmptySeq<TVO> $callback
+     * @psalm-return NonEmptySeq<TVO>
+     */
+    function flatMap(callable $callback): NonEmptySeq;
+
+    /**
+     * Do something for all collection elements
+     *
+     * @psalm-param callable(TV) $callback
+     */
+    function forAll(callable $callback): void;
+
+    /**
      * @psalm-return TV
      */
-    public function head(): mixed;
+    function head(): mixed;
+
+    /**
+     * Returns last collection element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    function last(callable $predicate): Option;
 
     /**
      * @template TVO
@@ -23,8 +133,32 @@ interface NonEmptySeqOps
     public function map(callable $callback): NonEmptySeq;
 
     /**
-     * @psalm-param callable(TV): bool $predicate
+     * Reduce multiple elements into one
+     *
+     * @psalm-param callable(TV, TV): TV $callback (accumulator, current value): new accumulator
+     * @psalm-return TV
+     */
+    function reduce(callable $callback): mixed;
+
+    /**
+     * Copy collection in reversed order
+     *
+     * @psalm-return NonEmptySeq<TV>
+     */
+    function reverse(): NonEmptySeq;
+
+    /**
+     * Returns every collection element except first
+     *
      * @psalm-return Seq<TV>
      */
-    public function filter(callable $predicate): Seq;
+    function tail(): Seq;
+
+    /**
+     * Returns collection unique elements
+     *
+     * @psalm-param null|(callable(TV): (int|string)) $callback returns element unique id
+     * @psalm-return NonEmptySeq<TV>
+     */
+    function unique(?callable $callback = null): NonEmptySeq;
 }
--- src/Fp/Collections/SeqOps.php
+++ src/Fp/Collections/SeqOps.php
@@ -13,9 +13,127 @@ use Fp\Functional\Option\Option;
 interface SeqOps
 {
     /**
+     * Returns true if there is collection element of given class
+     * False otherwise
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    function anyOf(string $fqcn, bool $invariant = false): bool;
+
+    /**
+     * Find element by its index
+     * Returns None if there is no such collection element
+     *
      * @psalm-return Option<TV>
      */
-    public function head(): Option;
+    function at(int $index): Option;
+
+    /**
+     * Returns true if every collection element satisfy the condition
+     * false otherwise
+     *
+     * @psalm-param callable(TV): bool $predicate
+     */
+    function every(callable $predicate): bool;
+
+    /**
+     * Returns true if every collection element is of given class
+     * false otherwise
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    function everyOf(string $fqcn, bool $invariant = false): bool;
+
+    /**
+     * Find if there is element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     */
+    function exists(callable $predicate): bool;
+
+    /**
+     * Filter collection by condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Seq<TV>
+     */
+    function filter(callable $predicate): Seq;
+
+    /**
+     * Filter not null elements
+     *
+     * @psalm-return Seq<TV>
+     */
+    function filterNotNull(): Seq;
+
+    /**
+     * Filter elements of given class
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     * @psalm-return Seq<TVO>
+     */
+    function filterOf(string $fqcn, bool $invariant = false): Seq;
+
+    /**
+     * Find first element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    function first(callable $predicate): Option;
+
+    /**
+     * Find first element of given class
+     *
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     * @psalm-return Option<TVO>
+     */
+    function firstOf(string $fqcn, bool $invariant = false): Option;
+
+    /**
+     * @psalm-template TVO
+     * @psalm-param callable(TV): Seq<TVO> $callback
+     * @psalm-return Seq<TVO>
+     */
+    function flatMap(callable $callback): Seq;
+
+    /**
+     * Fold many elements into one
+     *
+     * @psalm-template TA
+     * @psalm-param TA $init initial accumulator value
+     * @psalm-param callable(TV, TV): TV $callback (accumulator, current element): new accumulator
+     * @psalm-return TA|TV
+     */
+    function fold(mixed $init, callable $callback): mixed;
+
+    /**
+     * Do something for all collection elements
+     *
+     * @psalm-param callable(TV) $callback
+     */
+    function forAll(callable $callback): void;
+
+    /**
+     * @psalm-return Option<TV>
+     */
+    function head(): Option;
+
+    /**
+     * Returns last collection element which satisfies the condition
+     *
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    function last(callable $predicate): Option;
 
     /**
      * @template TVO
@@ -25,8 +143,33 @@ interface SeqOps
     public function map(callable $callback): Seq;
 
     /**
-     * @psalm-param callable(TV): bool $predicate
+     * Reduce multiple elements into one
+     * Returns None for empty collection
+     *
+     * @psalm-param callable(TV, TV): TV $callback (accumulator, current value): new accumulator
+     * @psalm-return Option<TV>
+     */
+    function reduce(callable $callback): Option;
+
+    /**
+     * Copy collection in reversed orderx
+     *
      * @psalm-return Seq<TV>
      */
-    public function filter(callable $predicate): Seq;
+    function reverse(): Seq;
+
+    /**
+     * Returns every collection element except first
+     *
+     * @psalm-return Seq<TV>
+     */
+    function tail(): Seq;
+
+    /**
+     * Returns collection unique elements
+     *
+     * @psalm-param null|(callable(TV): (int|string)) $callback returns element unique id
+     * @psalm-return Seq<TV>
+     */
+    function unique(?callable $callback = null): Seq;
 }
