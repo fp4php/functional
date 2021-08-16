8f6e603b83baac1d27ff688846e6f4c6de2117f1
whsv26 <whsv26@gmail.com>
2021-08-16 23:22:57 +0300

    Additional HashSet tests for object values.

--- tests/Runtime/Classes/HashSet/HashSetTest.php
+++ tests/Runtime/Classes/HashSet/HashSetTest.php
@@ -6,6 +6,8 @@ namespace Tests\Runtime\Classes\HashSet;
 
 use Fp\Collections\HashSet;
 use PHPUnit\Framework\TestCase;
+use Tests\Mock\Bar;
+use Tests\Mock\Foo;
 
 final class HashSetTest extends TestCase
 {
@@ -14,6 +16,24 @@ final class HashSetTest extends TestCase
         $this->assertEquals(
             [1, 2, 3],
             HashSet::collect([1, 1, 2, 3, 3])->toArray(),
+        );
+
+        $this->assertCount(
+            1,
+            HashSet::collect([new Foo(1), new Foo(1)])->toArray()
+        );
+
+        $this->assertCount(
+            2,
+            HashSet::collect([new Bar(1), new Bar(1)])->toArray()
+        );
+
+        $bar1 = new Bar(1);
+        $bar2 = $bar1;
+
+        $this->assertCount(
+            1,
+            HashSet::collect([$bar1, $bar2])->toArray()
         );
     }
 
