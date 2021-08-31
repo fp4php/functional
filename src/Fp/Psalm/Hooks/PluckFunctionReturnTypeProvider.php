39f8a419779ae5a4b6860d3b90df9afe0df7a3e1
whsv26 <whsv26@gmail.com>
2021-08-31 11:09:20 +0300

    Fix unique test.

--- src/Fp/Psalm/Hooks/PluckFunctionReturnTypeProvider.php
+++ src/Fp/Psalm/Hooks/PluckFunctionReturnTypeProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Fp\Psalm\Hooks;
 
-use Error;
 use Fp\Collections\ArrayList;
 use Fp\Functional\Option\Option;
 use Fp\Psalm\Psalm;
--- tests/Runtime/Functions/Collection/UniqueTest.php
+++ tests/Runtime/Functions/Collection/UniqueTest.php
@@ -13,27 +13,6 @@ final class UniqueTest extends TestCase
     public function testUnique(): void
     {
         $this->assertEquals(
-            [1, 2, 3, 4],
-            unique(
-                [1, 'a' => 2, 2, 3, 4],
-            )
-        );
-
-        $this->assertEquals(
-            [1],
-            unique(
-                [1],
-            )
-        );
-
-        $this->assertEquals(
-            [1, 2, 3],
-            unique(
-                [1, 1, 1, 1, 2, 3, 3],
-            )
-        );
-
-        $this->assertEquals(
             [1],
             unique(
                 [1, 1, 1, 1, 2, 3, 3],
