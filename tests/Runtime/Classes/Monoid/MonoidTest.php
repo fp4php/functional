4c9ec4ec7c61fd0119faf0ad265bd58390ef541a
whsv26 <whsv26@gmail.com>
2021-09-06 00:22:14 +0300

    Collection semigroup and monoid instance testing.

--- tests/Runtime/Classes/Monoid/MonoidTest.php
+++ tests/Runtime/Classes/Monoid/MonoidTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Tests\Runtime\Classes\Monoid;
 
+use Fp\Collections\ArrayList;
+use Fp\Collections\HashMap;
+use Fp\Collections\HashSet;
+use Fp\Collections\LinkedList;
+use Fp\Functional\Monoid\ArrayListMonoid;
 use Fp\Functional\Monoid\ArrayMonoid;
+use Fp\Functional\Monoid\HashMapMonoid;
+use Fp\Functional\Monoid\HashSetMonoid;
+use Fp\Functional\Monoid\LinkedListMonoid;
 use Fp\Functional\Monoid\ListMonoid;
-use Fp\Functional\Monoid\Monoid;
-use Fp\Functional\Semigroup\Semigroup;
 use PHPUnit\Framework\TestCase;
 
 final class MonoidTest extends TestCase
@@ -61,6 +67,86 @@ final class MonoidTest extends TestCase
         $this->assertEquals(
             [1, 2],
             $monoid->combine([1], [2])
+        );
+    }
+
+    public function testLinkedListMonoid(): void
+    {
+        $monoid = new LinkedListMonoid();
+
+        $this->assertEquals($monoid->empty(), $monoid->combine(
+            $monoid->empty(),
+            $monoid->empty()
+        ));
+
+        $this->assertEquals(
+            [1],
+            $monoid->combine(LinkedList::collect([1]), $monoid->empty())->toArray()
+        );
+
+        $this->assertEquals(
+            [1, 2],
+            $monoid->combine(LinkedList::collect([1]), LinkedList::collect([2]))->toArray()
+        );
+    }
+
+    public function testArrayListMonoid(): void
+    {
+        $monoid = new ArrayListMonoid();
+
+        $this->assertEquals($monoid->empty(), $monoid->combine(
+            $monoid->empty(),
+            $monoid->empty()
+        ));
+
+        $this->assertEquals(
+            [1],
+            $monoid->combine(ArrayList::collect([1]), $monoid->empty())->toArray()
+        );
+
+        $this->assertEquals(
+            [1, 2],
+            $monoid->combine(ArrayList::collect([1]), ArrayList::collect([2]))->toArray()
+        );
+    }
+
+    public function testHashSetMonoid(): void
+    {
+        $monoid = new HashSetMonoid();
+
+        $this->assertEquals($monoid->empty(), $monoid->combine(
+            $monoid->empty(),
+            $monoid->empty()
+        ));
+
+        $this->assertEquals(
+            [1],
+            $monoid->combine(HashSet::collect([1]), $monoid->empty())->toArray()
+        );
+
+        $this->assertEquals(
+            [1, 2],
+            $monoid->combine(HashSet::collect([1]), HashSet::collect([2]))->toArray()
+        );
+    }
+
+    public function testHashMapMonoid(): void
+    {
+        $monoid = new HashMapMonoid();
+
+        $this->assertEquals($monoid->empty(), $monoid->combine(
+            $monoid->empty(),
+            $monoid->empty()
+        ));
+
+        $this->assertEquals(
+            [['a', 1]],
+            $monoid->combine(HashMap::collect([['a', 1]]), $monoid->empty())->toArray()
+        );
+
+        $this->assertEquals(
+            [['a', 1], ['b', 2]],
+            $monoid->combine(HashMap::collect([['a', 1]]), HashMap::collect([['b', 2]]))->toArray()
         );
     }
 }
--- tests/Runtime/Classes/Semigroup/SemigroupTest.php
+++ tests/Runtime/Classes/Semigroup/SemigroupTest.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Tests\Runtime\Classes\Semigroup;
 
+use Fp\Collections\NonEmptyArrayList;
+use Fp\Collections\NonEmptyHashMap;
+use Fp\Collections\NonEmptyHashSet;
+use Fp\Collections\NonEmptyLinkedList;
 use Fp\Functional\Monoid\ListMonoid;
 use Fp\Functional\Monoid\Monoid;
 use Fp\Functional\Semigroup\LhsSemigroup;
+use Fp\Functional\Semigroup\NonEmptyArrayListSemigroup;
 use Fp\Functional\Semigroup\NonEmptyArraySemigroup;
+use Fp\Functional\Semigroup\NonEmptyHashMapSemigroup;
+use Fp\Functional\Semigroup\NonEmptyHashSetSemigroup;
+use Fp\Functional\Semigroup\NonEmptyLinkedListSemigroup;
 use Fp\Functional\Semigroup\NonEmptyListSemigroup;
 use Fp\Functional\Semigroup\RhsSemigroup;
 use Fp\Functional\Semigroup\Semigroup;
@@ -151,6 +159,50 @@ final class SemigroupTest extends TestCase
                 Validated::invalid(['err1']),
                 Validated::invalid(['err2']),
             )->get()
+        );
+    }
+
+    public function testLinkedListSemigroup(): void
+    {
+        /** @var Semigroup<NonEmptyLinkedList<int>> $semigroup */
+        $semigroup = new NonEmptyLinkedListSemigroup();
+
+        $this->assertEquals(
+            [1, 2],
+            $semigroup->combine(NonEmptyLinkedList::collectNonEmpty([1]), NonEmptyLinkedList::collectNonEmpty([2]))->toArray()
+        );
+    }
+
+    public function testArrayListSemigroup(): void
+    {
+        /** @var Semigroup<NonEmptyArrayList<int>> $semigroup */
+        $semigroup = new NonEmptyArrayListSemigroup();
+
+        $this->assertEquals(
+            [1, 2],
+            $semigroup->combine(NonEmptyArrayList::collectNonEmpty([1]), NonEmptyArrayList::collectNonEmpty([2]))->toArray()
+        );
+    }
+
+    public function testHashSetSemigroup(): void
+    {
+        /** @var Semigroup<NonEmptyHashSet<int>> $semigroup */
+        $semigroup = new NonEmptyHashSetSemigroup();
+
+        $this->assertEquals(
+            [1, 2],
+            $semigroup->combine(NonEmptyHashSet::collectNonEmpty([1]), NonEmptyHashSet::collectNonEmpty([2]))->toArray()
+        );
+    }
+
+    public function testHashMapSemigroup(): void
+    {
+        /** @var Semigroup<NonEmptyHashMap<string, int>> $semigroup */
+        $semigroup = new NonEmptyHashMapSemigroup();
+
+        $this->assertEquals(
+            [['a', 1], ['b', 2]],
+            $semigroup->combine(NonEmptyHashMap::collectNonEmpty([['a', 1]]), NonEmptyHashMap::collectNonEmpty([['b', 2]]))->toArray()
         );
     }
 }
