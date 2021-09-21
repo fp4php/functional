9f65a4a904ac12cbc021dc0f888edd5d29309649
whsv26 <whsv26@gmail.com>
2021-09-21 20:57:09 +0300

    Drop AbstractNonEmptySet.

--- src/Fp/Collections/NonEmptyHashSet.php
+++ src/Fp/Collections/NonEmptyHashSet.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Fp\Collections;
 
-use Error;
 use Fp\Functional\Option\Option;
 use Iterator;
 
@@ -14,9 +13,9 @@ use function Fp\of;
 /**
  * @template-covariant TV
  * @psalm-immutable
- * @extends AbstractNonEmptySet<TV>
+ * @implements NonEmptySet<TV>
  */
-final class NonEmptyHashSet extends AbstractNonEmptySet
+final class NonEmptyHashSet implements NonEmptySet
 {
     /**
      * @internal
@@ -68,6 +67,71 @@ final class NonEmptyHashSet extends AbstractNonEmptySet
 
     /**
      * @inheritDoc
+     */
+    public function count(): int
+    {
+        $counter = 0;
+
+        foreach ($this as $ignored) {
+            $counter++;
+        }
+
+        return $counter;
+    }
+
+    /**
+     * @inheritDoc
+     * @return non-empty-list<TV>
+     */
+    public function toArray(): array
+    {
+        $buffer = [$this->head()];
+
+        foreach ($this->tail() as $elem) {
+            $buffer[] = $elem;
+        }
+
+        return $buffer;
+    }
+
+    /**
+     * @inheritDoc
+     * @return LinkedList<TV>
+     */
+    public function toLinkedList(): LinkedList
+    {
+        return LinkedList::collect($this);
+    }
+
+    /**
+     * @inheritDoc
+     * @return ArrayList<TV>
+     */
+    public function toArrayList(): ArrayList
+    {
+        return ArrayList::collect($this);
+    }
+
+    /**
+     * @inheritDoc
+     * @return NonEmptyLinkedList<TV>
+     */
+    public function toNonEmptyLinkedList(): NonEmptyLinkedList
+    {
+        return NonEmptyLinkedList::collectUnsafe($this);
+    }
+
+    /**
+     * @inheritDoc
+     * @return NonEmptyArrayList<TV>
+     */
+    public function toNonEmptyArrayList(): NonEmptyArrayList
+    {
+        return NonEmptyArrayList::collectUnsafe($this);
+    }
+
+    /**
+     * @inheritDoc
      * @return HashSet<TV>
      */
     public function toHashSet(): HashSet
@@ -82,6 +146,218 @@ final class NonEmptyHashSet extends AbstractNonEmptySet
     public function toNonEmptyHashSet(): NonEmptyHashSet
     {
         return $this;
+    }
+
+    /**
+     * @inheritDoc
+     * @template TKI
+     * @template TVI
+     * @param callable(TV): array{TKI, TVI} $callback
+     * @return HashMap<TKI, TVI>
+     */
+    public function toHashMap(callable $callback): HashMap
+    {
+        return HashMap::collectPairs(asGenerator(function () use ($callback) {
+            foreach ($this as $elem) {
+                yield $callback($elem);
+            }
+        }));
+    }
+
+    /**
+     * @inheritDoc
+     * @template TKI
+     * @template TVI
+     * @param callable(TV): array{TKI, TVI} $callback
+     * @return NonEmptyHashMap<TKI, TVI>
+     */
+    public function toNonEmptyHashMap(callable $callback): NonEmptyHashMap
+    {
+        return NonEmptyHashMap::collectPairsUnsafe(asGenerator(function () use ($callback) {
+            foreach ($this as $elem) {
+                yield $callback($elem);
+            }
+        }));
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-param TV $element
+     */
+    public function __invoke(mixed $element): bool
+    {
+        return $this->contains($element);
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-param callable(TV): bool $predicate
+     */
+    public function every(callable $predicate): bool
+    {
+        $result = true;
+
+        foreach ($this as $element) {
+            if (!$predicate($element)) {
+                $result = false;
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    public function everyOf(string $fqcn, bool $invariant = false): bool
+    {
+        return $this->every(fn(mixed $v) => of($v, $fqcn, $invariant));
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-param callable(TV): bool $predicate
+     */
+    public function exists(callable $predicate): bool
+    {
+        return $this->first($predicate)->isSome();
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     */
+    public function existsOf(string $fqcn, bool $invariant = false): bool
+    {
+        return $this->firstOf($fqcn, $invariant)->fold(
+            fn() => true,
+            fn() => false,
+        );
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    public function first(callable $predicate): Option
+    {
+        $first = null;
+
+        foreach ($this as $element) {
+            if ($predicate($element)) {
+                $first = $element;
+                break;
+            }
+        }
+
+        return Option::fromNullable($first);
+    }
+
+
+    /**
+     * @inheritDoc
+     * @psalm-param callable(TV): bool $predicate
+     * @psalm-return Option<TV>
+     */
+    public function last(callable $predicate): Option
+    {
+        $last = null;
+
+        foreach ($this as $element) {
+            if ($predicate($element)) {
+                $last = $element;
+            }
+        }
+
+        return Option::fromNullable($last);
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-template TVO
+     * @psalm-param class-string<TVO> $fqcn fully qualified class name
+     * @psalm-param bool $invariant if turned on then subclasses are not allowed
+     * @psalm-return Option<TVO>
+     */
+    public function firstOf(string $fqcn, bool $invariant = false): Option
+    {
+        /** @var Option<TVO> */
+        return $this->first(fn(mixed $v): bool => of($v, $fqcn, $invariant));
+    }
+
+    /**
+     * @inheritDoc
+     * @template TA
+     * @psalm-param callable(TV|TA, TV): (TV|TA) $callback
+     * @psalm-return (TV|TA)
+     */
+    public function reduce(callable $callback): mixed
+    {
+        $acc = $this->head();
+
+        foreach ($this->tail() as $element) {
+            $acc = $callback($acc, $element);
+        }
+
+        return $acc;
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-return TV
+     */
+    public function head(): mixed
+    {
+        $head = null;
+
+        foreach ($this as $element) {
+            $head = $element;
+            break;
+        }
+
+        return Option::fromNullable($head)->getUnsafe();
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-return TV
+     */
+    public function firstElement(): mixed
+    {
+        return $this->head();
+    }
+
+    /**
+     * @inheritDoc
+     * @psalm-return TV
+     */
+    public function lastElement(): mixed
+    {
+        return $this->last(fn() => true)->getUnsafe();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function subsetOf(Set|NonEmptySet $superset): bool
+    {
+        $isSubset = true;
+
+        foreach ($this as $elem) {
+            if (!$superset($elem)) {
+                $isSubset = false;
+                break;
+            }
+        }
+
+        return $isSubset;
     }
 
     /**
