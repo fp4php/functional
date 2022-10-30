0dcb081f39982373b65a3f4ee1fc4eb535b8764d
adrew <klimichkartorgnusov@gmail.com>
2022-10-30 12:47:47 +0300

    Cover all extensible classes in ExtensionTest

--- tests/Runtime/Classes/ExtensionTest.php
+++ tests/Runtime/Classes/ExtensionTest.php
@@ -5,7 +5,30 @@ declare(strict_types=1);
 namespace Tests\Runtime\Classes;
 
 use Fp\Collections\ArrayList;
+use Fp\Collections\HashMap;
+use Fp\Collections\HashSet;
+use Fp\Collections\LinkedList;
+use Fp\Collections\NonEmptyArrayList;
+use Fp\Collections\NonEmptyHashMap;
+use Fp\Collections\NonEmptyHashSet;
+use Fp\Collections\NonEmptyLinkedList;
+use Fp\Functional\Either\Either;
+use Fp\Functional\Option\Option;
+use Fp\Functional\Separated\Separated;
+use Fp\Streams\Stream;
 use Fp\Collections\ArrayListExtensions;
+use Fp\Collections\HashMapExtensions;
+use Fp\Collections\HashSetExtensions;
+use Fp\Collections\LinkedListExtensions;
+use Fp\Collections\NonEmptyArrayListExtensions;
+use Fp\Collections\NonEmptyHashMapExtensions;
+use Fp\Collections\NonEmptyHashSetExtensions;
+use Fp\Collections\NonEmptyLinkedListExtensions;
+use Fp\Functional\Either\EitherExtensions;
+use Fp\Functional\Option\OptionExtensions;
+use Fp\Functional\Separated\SeparatedExtensions;
+use Fp\Streams\StreamExtensions;
+
 use PHPUnit\Framework\TestCase;
 
 final class ExtensionTest extends TestCase
@@ -16,31 +39,100 @@ final class ExtensionTest extends TestCase
     /** @var non-empty-string  */
     private string $testStaticMethodName = 'testStaticMethod';
 
+    /**
+     * @noinspection PhpUndefinedMethodInspection
+     */
     protected function setUp(): void
     {
-        ArrayListExtensions::addInstanceExtension($this->testMethodName, function(ArrayList $list) {
-            /** @var ArrayList<int> $list */;
-            return $list->fold(0)(fn($acc, $cur) => $acc + $cur);
-        });
+        $extensions = [
+            ArrayListExtensions::class,
+            LinkedListExtensions::class,
+            HashSetExtensions::class,
+            HashMapExtensions::class,
+            NonEmptyArrayListExtensions::class,
+            NonEmptyLinkedListExtensions::class,
+            NonEmptyHashSetExtensions::class,
+            NonEmptyHashMapExtensions::class,
+            StreamExtensions::class,
+            OptionExtensions::class,
+            EitherExtensions::class,
+            SeparatedExtensions::class,
+        ];
 
-        ArrayListExtensions::addStaticExtension($this->testStaticMethodName, function(string $string): ArrayList {
-            return ArrayList::collect(str_split($string));
-        });
+        foreach ($extensions as $extension) {
+            $extension::addInstanceExtension($this->testMethodName, fn() => 42);
+            $extension::addStaticExtension($this->testStaticMethodName, fn() => 42);
+        }
     }
 
+    /**
+     * @noinspection PhpUndefinedMethodInspection
+     */
     protected function tearDown(): void
     {
-        ArrayListExtensions::removeInstanceExtension($this->testMethodName);
-        ArrayListExtensions::removeStaticExtension($this->testStaticMethodName);
+        $extensions = [
+            ArrayListExtensions::class,
+            LinkedListExtensions::class,
+            HashSetExtensions::class,
+            HashMapExtensions::class,
+            NonEmptyArrayListExtensions::class,
+            NonEmptyLinkedListExtensions::class,
+            NonEmptyHashSetExtensions::class,
+            NonEmptyHashMapExtensions::class,
+            StreamExtensions::class,
+            OptionExtensions::class,
+            EitherExtensions::class,
+            SeparatedExtensions::class,
+        ];
+
+        foreach ($extensions as $extension) {
+            $extension::removeInstanceExtension($this->testMethodName);
+            $extension::removeStaticExtension($this->testStaticMethodName);
+        }
     }
 
-    public function testAddInstanceMethod(): void
+    public function providedExtensibleClasses(): array
     {
-        /** @psalm-suppress MixedAssignment */
-        $actual = ArrayList::collect([1, 2, 3])->{$this->testMethodName}();
+        return [
+            [ArrayList::singleton(1), ArrayListExtensions::class],
+            [LinkedList::singleton(1), LinkedListExtensions::class],
+            [HashSet::singleton(1), HashSetExtensions::class],
+            [HashMap::collect([1]), HashMapExtensions::class],
+            [NonEmptyArrayList::singleton(1), NonEmptyArrayListExtensions::class],
+            [NonEmptyLinkedList::singleton(1), NonEmptyLinkedListExtensions::class],
+            [NonEmptyHashSet::singleton(1), NonEmptyHashSetExtensions::class],
+            [NonEmptyHashMap::collectNonEmpty([1]), NonEmptyHashMapExtensions::class],
+            [Stream::emit(1), StreamExtensions::class],
+            [Option::some(1), OptionExtensions::class],
+            [Either::right(1), EitherExtensions::class],
+            [Separated::create(1, 2), SeparatedExtensions::class],
+        ];
+    }
 
-        $this->assertEquals(6, $actual);
-        $this->assertArrayHasKey($this->testMethodName, ArrayListExtensions::getAllInstanceExtensions());
+    /**
+     * @dataProvider providedExtensibleClasses
+     * @noinspection PhpUndefinedMethodInspection
+     * @psalm-suppress all
+     */
+    public function testCallInstanceExtensionMethod(object $instance, string $extClass): void
+    {
+        $actual = $instance->{$this->testMethodName}();
+
+        $this->assertEquals(42, $actual);
+        $this->assertArrayHasKey($this->testMethodName, $extClass::getAllInstanceExtensions());
+    }
+
+    /**
+     * @dataProvider providedExtensibleClasses
+     * @noinspection PhpUndefinedMethodInspection
+     * @psalm-suppress all
+     */
+    public function testCallStaticExtensionMethod(object $instance, string $extClass): void
+    {
+        $actual = ($instance::class)::{$this->testStaticMethodName}();
+
+        $this->assertEquals(42, $actual);
+        $this->assertArrayHasKey($this->testStaticMethodName, $extClass::getAllStaticExtensions());
     }
 
     public function testAddInstanceMethodTwice(): void
@@ -51,15 +143,6 @@ final class ExtensionTest extends TestCase
             /** @var ArrayList<int> $list */;
             return $list->fold(0)(fn($acc, $cur) => $acc + $cur);
         });
-    }
-
-    public function testAddStaticMethod(): void
-    {
-        /** @psalm-suppress MixedAssignment */
-        $actual = ArrayList::{$this->testStaticMethodName}('abc');
-
-        $this->assertArrayHasKey($this->testStaticMethodName, ArrayListExtensions::getAllStaticExtensions());
-        $this->assertEquals(ArrayList::collect(['a', 'b', 'c']), $actual);
     }
 
     public function testAddStaticMethodTwice(): void
